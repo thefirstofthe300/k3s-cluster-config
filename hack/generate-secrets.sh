dda465694f39b91dcc8fa5aef35c29b40df01745
Danny Seymour <danny@seymour.family>
2023-05-14 07:59:28 +0000

    fix: deploy Immich

--- hack/generate-secrets.sh
+++ hack/generate-secrets.sh
@@ -1,5 +1,8 @@
-kubeseal -f secrets/synapse/bridges/mautrix-signal/secret.yaml -o yaml > synapse/bridges/mautrix-signal/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mautrix-telegram/secret.yaml -o yaml > synapse/bridges/mautrix-telegram/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mautrix-discord/secret.yaml -o yaml > synapse/bridges/mautrix-discord/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/mx-puppet-groupme/secret.yaml -o yaml > synapse/bridges/mx-puppet-groupme/sealedsecret.yaml
-kubeseal -f secrets/synapse/bridges/registrations/secret.yaml -o yaml > synapse/bridges/registrations/sealedsecret.yaml
+#! /usr/bin/env bash
+
+kubeseal -f secrets/synapse/bridges/mautrix-signal/secret.yaml -o yaml | grep -v creationTimestamp > synapse/bridges/mautrix-signal/sealedsecret.yaml
+kubeseal -f secrets/synapse/bridges/mautrix-telegram/secret.yaml -o yaml | grep -v creationTimestamp > synapse/bridges/mautrix-telegram/sealedsecret.yaml
+kubeseal -f secrets/synapse/bridges/mautrix-discord/secret.yaml -o yaml | grep -v creationTimestamp > synapse/bridges/mautrix-discord/sealedsecret.yaml
+kubeseal -f secrets/synapse/bridges/mx-puppet-groupme/secret.yaml -o yaml | grep -v creationTimestamp > synapse/bridges/mx-puppet-groupme/sealedsecret.yaml
+kubeseal -f secrets/synapse/bridges/registrations/secret.yaml -o yaml | grep -v creationTimestamp > synapse/bridges/registrations/sealedsecret.yaml
+kubeseal -f secrets/immich/secret.yaml -o yaml | grep -v creationTimestamp > immich/sealedsecret.yaml
